9b27ea14036af60da9cc651b7adae98a67e589f1
unknown <kennedy.julio@academico.ifpb.edu.br>
2024-12-26 00:29:48 -0300

    1.0.1

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 let lua = document.getElementById('lua')
 let body = document.querySelector('body')
-let dado = document.getElementById("dados")
-let front = document.getElementById("front")
+let dado = document.getElementById('dados')
+let front = document.getElementById('front')
+let tec = document.getElementById('tec')
 
 
 lua.addEventListener('click', ()=>{
     body.classList.toggle('dark')
     dado.classList.toggle('dark')
     front.classList.toggle('dark')
+    tec.classList.toggle('dark')
     if (lua.classList.contains('fa-moon')) {
         lua.classList.remove('fa-moon');
         lua.classList.add('fa-sun');

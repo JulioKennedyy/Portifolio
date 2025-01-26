02ff2a631d54c868345d7ac39e6a42d60d36aad0
unknown <kennedy.julio@academico.ifpb.edu.br>
2025-01-25 23:23:59 -0300

    1.0.4

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-let lua = document.getElementById('lua')
+let lua = document.getElementById('luaa')
 let body = document.querySelector('body')
 let dado = document.getElementById('dados')
 let front = document.getElementById('front')
 let tec = document.getElementById('tec')
-let html = document.getElementById('htmll')
 
 
 lua.addEventListener('click', ()=>{
@@ -11,13 +10,15 @@ lua.addEventListener('click', ()=>{
     dado.classList.toggle('dark')
     front.classList.toggle('dark')
     tec.classList.toggle('dark')
-    html.classList.toggle('dark')
+    console.log('Clique detectado!');
     if (lua.classList.contains('fa-moon')) {
         lua.classList.remove('fa-moon');
         lua.classList.add('fa-sun');
+        console.log('Mudou para sol');
     } else {
         lua.classList.remove('fa-sun');
         lua.classList.add('fa-moon');
+        console.log('Mudou para lua');
     }
 });
 
